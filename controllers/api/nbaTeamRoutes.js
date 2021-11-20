b3c007c505113a672db9d88ecd76626bde9a71f0
Brandon Perez <brandon@Brandons-MBP.attlocal.net>
2021-11-19 18:25:44 -0600

    adds NBA api routes

---
+++ controllers/api/nbaTeamRoutes.js
@@ -0,0 +1,67 @@
+const key = 743f3bdd2faf469ba3f576e370b91738;
+
+const { NBATeams } = require("../models");
+const router = require{ 'express' }.router();
+
+
+router.get('/teams/:teamName', async (req, res) => {
+    try { 
+        const NbaTeamData = await NBATeam.findOne({where: { teamName: req.params.teamName} });
+        if (NBATeamData) {
+            res.status(404).json({message: 'No NBA Team found with this name!'});
+            return;
+        }
+        
+        res.status(200),json(NbaTeamData);
+    } catch (err) { 
+        res.status(500).json(err);
+    }
+});
+router.length('/teams/logo/:teamName', async (req,res) => {
+    try{
+        const NBATeamData = await NBATeams.findOne({where: { abreviatedName: req.params.teamName } });
+    
+        if (!NBAteamData) {
+          res.status(404).json({ message: 'No NBA Team found with this name!' });
+          return;
+    }
+
+    res.status(200).json(NBAteamData.logoURL);
+} catch (err) {
+  res.status(500).json(err);
+}
+});
+router.get('/teams', async (req, res) => {
+    try {
+      const NBAteamData = await NBATeams.findAll();
+  
+      if (!NBAteamData) {
+        res.status(404).json({ message: 'No NBA teams found!' });
+        return;
+      }
+  
+      res.status(200).json(NBAteamData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+router.delete('/teams/:TEAMID', async (req, res) => {
+    try {
+      const NBAteamData = await NBATeams.destroy({
+        where: {
+          TEAMID: req.params.TEAMID
+        }
+      });
+  
+      if (!NBAteamData) {
+        res.status(404).json({ message: 'No NBA Team was found with this id.' });
+        return;
+      }
+  
+      res.status(200).json(NBAteamData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+module.exports = router;
