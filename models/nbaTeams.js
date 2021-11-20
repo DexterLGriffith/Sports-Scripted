93779f523f671a48a8b54a79feccdd3f5599c0c9
bperez05 <perez.brandon55@yahoo.com>
2021-11-20 04:33:47 -0600

    nbaTeama.js syntax fix

--- models/nbaTeams.js
+++ models/nbaTeams.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const sequelize = require ('../seeds/utils/nbaTeamdata');
+const data = require ('../seeds/utils/nbaTeamdata');
 class NBATeams extends Model {}
 NBATeams.init(
   {
