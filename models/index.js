6a2037b799735a17b453ca69e2921dd18d821af8
bperez05 <perez.brandon55@yahoo.com>
2022-01-10 23:51:19 -0600

    adds seeds files'

--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
 const User = require('./User');
+const NBATeams = require('./nbaTeams')
 
-module.exports = { User };
+
+module.exports = { NBATeams, User };
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,14 @@
+const sequelize = require('../config/connection');
+const{ NBATeams } = require('../models');
 
+const NBAteamsSeedData = require('./nbaSeeds.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: false });
+
+    const NBAteams = await NBATeams.bulkCreate(NBAteamsSeedData);
+
+    process.exit(1);
+
+};
+seedDatabase();
