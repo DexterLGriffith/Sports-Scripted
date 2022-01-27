fa75ec9159b5e5a9231f30892cb7b9ae15652daa
bperez05 <perez.brandon55@yahoo.com>
2022-01-26 21:53:32 -0600

    edited synatx in models'

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,10 @@ const router = require('express').Router();
 const nbaTeamRoutes = require('./nbaTeamRoutes');
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+// const homeRoutes = require('./homeRoutes');
 
-router.use('/', homeRoutes);
+// router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('api/nba', nbaTeamRoutes);
 
 module.exports = router;
